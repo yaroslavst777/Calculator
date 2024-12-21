d20c5f66845a6c99f80b48a592bec1928333cd2e
yaroslavst777 <yaroslav.2009.may@gmail.com>
2024-12-22 01:14:52 +0300

    Update errors.go
    removed spaces

--- pkg/calculation/errors.go
+++ pkg/calculation/errors.go
@@ -3,7 +3,7 @@ package calculation
 import "errors"
 
 var (
-	ErrInvalidExpression     = errors.New("invalid expression")
-	ErrDivisionByZero        = errors.New("division by zero")
+	ErrInvalidExpression = errors.New("invalid expression")
+	ErrDivisionByZero = errors.New("division by zero")
 	ErrMismatchedParentheses = errors.New("mismatched parentheses")
 )
